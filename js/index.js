c41a6abab289b9a951d4d9ddb067a92b5eb40e07
Patrick Roach <patrickevanroach@gmail.com>
2019-12-02 17:13:32 -0700

    completed mvp

--- js/index.js
+++ js/index.js
@@ -40,3 +40,68 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let nav = document.querySelector("nav");
+
+
+let navItems = document.querySelectorAll('a');
+// let navItemOne = document
+
+console.log(navItems[0]);
+
+navItems[0].textContent = "Services";
+navItems[1].textContent = "Product";
+navItems[2].textContent = "Vision";
+navItems[3].textContent = "Features";
+navItems[4].textContent = "About";
+navItems[5].textContent = "Contact";
+
+let ctaDumbName = document.querySelectorAll(".cta");
+let ctaH = document.querySelector("h1");
+let buttons = document.querySelectorAll("button")
+
+ctaH.textContent = "Dom Is Awesome";
+
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+buttons[0].textContent = "Get Started";
+
+let hFour = document.querySelectorAll("h4")
+
+hFour[0].textContent= "Features";
+hFour[1].textContent="About";
+hFour[2].textContent="Services"
+hFour[3].textContent="Product"
+hFour[4].textContent="Vision"
+
+let p = document.querySelectorAll("p")
+
+p[0].textContent="Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+p[1].textContent="About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+p[2].textContent="Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+p[3].textContent="Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+p[4].textContent="Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+hFour[5].textContent="Contact"
+p[5].textContent="123 Way 456 Street Somewhere, USA"
+p[6].textContent="1 (888) 888-8888"
+p[7].textContent="sales@greatidea.io"
+
+//footer
+
+p[8].textContent="Copyright Great Idea! 2018"
+
+navItems.forEach(elem=>(
+  elem.style.color = "green"
+))
+
+let atTheEnd = document.createElement("a");
+let inTheBeg = document.createElement("a")
+
+nav.appendChild(atTheEnd);
+nav.prepend(inTheBeg);
